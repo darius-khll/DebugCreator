aded7747aca0e3901d5a25c4ad686d9bea366dea
Ali Khalili <ali.khll.1994@gmail.com>
2017-02-26 11:54:36 +0330

    debug creator instnace added

--- index.ts
+++ index.ts
@@ -6,17 +6,21 @@
 import * as fs from "fs";
 import { fileSystem } from "./services/fileSystem"
 
+// let userArgs = process.argv.slice(2);
+// let givenArguments: { Type: string } = {} as any;
+// switch (userArgs.length) {
+//     case 0: throw Error('err1');
+//     case 1:
+//         givenArguments.Type = userArgs[0];
+//         break;
+// }
 
-
-
-
-let userArgs = process.argv.slice(2);
-let givenArguments: { Name: string } = {} as any;
-switch (userArgs.length) {
-    case 0: throw Error('err1');
-    case 1:
-        givenArguments.Name = userArgs[0];
-        break;
+class Creator {
+    public async debugCreator(): Promise<void> {
+        let data = await fileSystem.readFile("txt.txt");
+        var lines = data.split("\r\n");
+        console.log(lines[0] + " - " + lines[1]);
+    }
 }
 
-console.log(`debugger creator`);
+new Creator().debugCreator();
