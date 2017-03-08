a0781a85c56e83f57b7b3f2f034952671fd4b881
Ali Khalili <ali.khll.1994@gmail.com>
2017-03-08 18:55:34 +0330

    index file fixed to initial function handler + code enhancement

--- index.ts
+++ index.ts
@@ -6,36 +6,48 @@
 import { fileSystem } from "./services/fileSystem"
 import { Creator } from "./services/debugger"
 
-let userArgs = process.argv.slice(2);
-let givenArguments: { FileName: string, Type: string, Firstline: number, LastLine: number } = {} as any;
+function initial() {
 
-switch (userArgs.length) {
-    case 0: throw Error('could not have any parameters!');
-    case 1:
-        if (userArgs[0].toLowerCase() == "--help") {
-            console.log("help");
+    let userArgs = process.argv.slice(2);
+    //let userArgs = ["txt.txt"];
+
+    let givenArguments: { FileName: string, Type: string, Firstline: number, LastLine: number } = {} as any;
+
+    switch (userArgs.length) {
+        case 0: throw Error('could not have any parameters!');
+        case 1:
+            if (userArgs[0].toLowerCase() == "--help") {
+                console.log("help");
+                break;
+            }
+            console.log(111);
             break;
-        }
-        givenArguments.FileName = userArgs[0];
-        break;
-    case 2:
-        givenArguments.FileName = userArgs[0];
-        givenArguments.Type = userArgs[1];
-        break;
-    case 3:
-        givenArguments.FileName = userArgs[0];
-        givenArguments.Firstline = parseInt(userArgs[1]);
-        givenArguments.LastLine = parseInt(userArgs[2]);
-        break;
-    case 4:
-        givenArguments.FileName = userArgs[0];
-        givenArguments.Firstline = parseInt(userArgs[1]);
-        givenArguments.LastLine = parseInt(userArgs[2]);
-        givenArguments.Type = userArgs[3];
-        break;
+        case 2:
+            givenArguments.FileName = userArgs[0];
+            givenArguments.Type = userArgs[1];
+            break;
+        case 3:
+            givenArguments.FileName = userArgs[0];
+            givenArguments.Firstline = parseInt(userArgs[1]);
+            givenArguments.LastLine = parseInt(userArgs[2]);
+            break;
+        case 4:
+            givenArguments.FileName = userArgs[0];
+            givenArguments.Firstline = parseInt(userArgs[1]);
+            givenArguments.LastLine = parseInt(userArgs[2]);
+            givenArguments.Type = userArgs[3];
+            break;
+    }
+
+    console.log(`File name is: ${userArgs[0]}`);
+
+    if (userArgs[0].toLowerCase() != "--help") {
+        let creator = new Creator(givenArguments.FileName, givenArguments.Firstline, givenArguments.LastLine);
+        creator.debugCreator().then((data) => {
+            console.log("finished...");
+        });
+    }
+
 }
 
-
-
-if (userArgs[0].toLowerCase() != "--help")
-    new Creator(givenArguments.FileName, givenArguments.Firstline, givenArguments.LastLine).debugCreator();
+initial();
